578ab4afc37ad569a1809c7daad23b9cc0b60602
Kilian Sweeney <kilian_sweeney@yahoo.com>
2019-11-03 09:04:01 -0600

    no message

---
+++ build/precache-manifest.1e5fc0dc269dde2767781a7f7e9a2e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114892e36d54ad3446b8e920b02f2916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "819d22b4ce56625d42bf",
+    "url": "/static/css/main.ca5faae9.chunk.css"
+  },
+  {
+    "revision": "f0f1bdc617775482dacf",
+    "url": "/static/js/2.47bb5b45.chunk.js"
+  },
+  {
+    "revision": "819d22b4ce56625d42bf",
+    "url": "/static/js/main.b414fcff.chunk.js"
+  },
+  {
+    "revision": "cb11dfd74a2d4214e7c0",
+    "url": "/static/js/runtime-main.c793ec90.js"
+  }
+]);
